c82aef5edfcc061a2a3bdc35ab65a50691c3f852
ChineseHuang <1084342410@qq.com>
2017-06-03 17:10:02 +0800

    C#,ArcEngine
    实现了ArcGIS的部分功能

---
+++ GMap/MainForm.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace GMap
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tool_zoomin = new System.Windows.Forms.ToolStripButton();
+            this.tool_zoomout = new System.Windows.Forms.ToolStripButton();
+            this.tool_pan = new System.Windows.Forms.ToolStripButton();
+            this.btnOpenMap = new System.Windows.Forms.Button();
+            this.btnAddData = new System.Windows.Forms.Button();
+            this.btnRender = new System.Windows.Forms.Button();
+            this.btnTextElement = new System.Windows.Forms.Button();
+            this.btnSpatialQuery = new System.Windows.Forms.Button();
+            this.btnAttributeQuery = new System.Windows.Forms.Button();
+            this.axToolbarControl1 = new ESRI.ArcGIS.Controls.AxToolbarControl();
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.axTOCControl1 = new ESRI.ArcGIS.Controls.AxTOCControl();
+            this.txtFieldName = new System.Windows.Forms.TextBox();
+            this.listFields = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(677, 326);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 2;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tool_zoomin,
+            this.tool_zoomout,
+            this.tool_pan});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(975, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tool_zoomin
+            // 
+            this.tool_zoomin.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tool_zoomin.Image = ((System.Drawing.Image)(resources.GetObject("tool_zoomin.Image")));
+            this.tool_zoomin.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tool_zoomin.Name = "tool_zoomin";
+            this.tool_zoomin.Size = new System.Drawing.Size(23, 22);
+            this.tool_zoomin.Text = "toolStripButton1";
+            this.tool_zoomin.Click += new System.EventHandler(this.tool_zoomin_Click);
+            // 
+            // tool_zoomout
+            // 
+            this.tool_zoomout.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tool_zoomout.Image = ((System.Drawing.Image)(resources.GetObject("tool_zoomout.Image")));
+            this.tool_zoomout.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tool_zoomout.Name = "tool_zoomout";
+            this.tool_zoomout.Size = new System.Drawing.Size(23, 22);
+            this.tool_zoomout.Text = "toolStripButton2";
+            this.tool_zoomout.Click += new System.EventHandler(this.tool_zoomout_Click);
+            // 
+            // tool_pan
+            // 
+            this.tool_pan.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tool_pan.Image = ((System.Drawing.Image)(resources.GetObject("tool_pan.Image")));
+            this.tool_pan.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tool_pan.Name = "tool_pan";
+            this.tool_pan.Size = new System.Drawing.Size(23, 22);
+            this.tool_pan.Text = "toolStripButton3";
+            this.tool_pan.Click += new System.EventHandler(this.tool_pan_Click);
+            // 
+            // btnOpenMap
+            // 
+            this.btnOpenMap.Location = new System.Drawing.Point(6, 26);
+            this.btnOpenMap.Name = "btnOpenMap";
+            this.btnOpenMap.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenMap.TabIndex = 6;
+            this.btnOpenMap.Text = "打开地图";
+            this.btnOpenMap.UseVisualStyleBackColor = true;
+            this.btnOpenMap.Click += new System.EventHandler(this.btnOpenMap_Click);
+            // 
+            // btnAddData
+            // 
+            this.btnAddData.Location = new System.Drawing.Point(99, 26);
+            this.btnAddData.Name = "btnAddData";
+            this.btnAddData.Size = new System.Drawing.Size(75, 23);
+            this.btnAddData.TabIndex = 7;
+            this.btnAddData.Text = "加载数据";
+            this.btnAddData.UseVisualStyleBackColor = true;
+            this.btnAddData.Click += new System.EventHandler(this.btnAddData_Click);
+            // 
+            // btnRender
+            // 
+            this.btnRender.Location = new System.Drawing.Point(192, 26);
+            this.btnRender.Name = "btnRender";
+            this.btnRender.Size = new System.Drawing.Size(75, 23);
+            this.btnRender.TabIndex = 8;
+            this.btnRender.Text = "地图渲染";
+            this.btnRender.UseVisualStyleBackColor = true;
+            this.btnRender.Click += new System.EventHandler(this.btnRender_Click);
+            // 
+            // btnTextElement
+            // 
+            this.btnTextElement.Location = new System.Drawing.Point(285, 26);
+            this.btnTextElement.Name = "btnTextElement";
+            this.btnTextElement.Size = new System.Drawing.Size(75, 23);
+            this.btnTextElement.TabIndex = 9;
+            this.btnTextElement.Text = "文本标注";
+            this.btnTextElement.UseVisualStyleBackColor = true;
+            this.btnTextElement.Click += new System.EventHandler(this.btnTextElement_Click);
+            // 
+            // btnSpatialQuery
+            // 
+            this.btnSpatialQuery.Location = new System.Drawing.Point(378, 26);
+            this.btnSpatialQuery.Name = "btnSpatialQuery";
+            this.btnSpatialQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnSpatialQuery.TabIndex = 10;
+            this.btnSpatialQuery.Text = "空间查询";
+            this.btnSpatialQuery.UseVisualStyleBackColor = true;
+            this.btnSpatialQuery.Click += new System.EventHandler(this.btnSpatialQuery_Click);
+            // 
+            // btnAttributeQuery
+            // 
+            this.btnAttributeQuery.Location = new System.Drawing.Point(869, 90);
+            this.btnAttributeQuery.Name = "btnAttributeQuery";
+            this.btnAttributeQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnAttributeQuery.TabIndex = 11;
+            this.btnAttributeQuery.Text = "属性查询";
+            this.btnAttributeQuery.UseVisualStyleBackColor = true;
+            this.btnAttributeQuery.Click += new System.EventHandler(this.btnAttributeQuery_Click);
+            // 
+            // axToolbarControl1
+            // 
+            this.axToolbarControl1.Location = new System.Drawing.Point(677, 58);
+            this.axToolbarControl1.Name = "axToolbarControl1";
+            this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
+            this.axToolbarControl1.Size = new System.Drawing.Size(154, 28);
+            this.axToolbarControl1.TabIndex = 4;
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(190, 53);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(648, 438);
+            this.axMapControl1.TabIndex = 3;
+            this.axMapControl1.OnMouseDown += new ESRI.ArcGIS.Controls.IMapControlEvents2_Ax_OnMouseDownEventHandler(this.axMapControl1_OnMouseDown);
+            // 
+            // axTOCControl1
+            // 
+            this.axTOCControl1.Location = new System.Drawing.Point(3, 53);
+            this.axTOCControl1.Name = "axTOCControl1";
+            this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
+            this.axTOCControl1.Size = new System.Drawing.Size(183, 438);
+            this.axTOCControl1.TabIndex = 1;
+            // 
+            // txtFieldName
+            // 
+            this.txtFieldName.Location = new System.Drawing.Point(869, 53);
+            this.txtFieldName.Name = "txtFieldName";
+            this.txtFieldName.Size = new System.Drawing.Size(75, 21);
+            this.txtFieldName.TabIndex = 12;
+            // 
+            // listFields
+            // 
+            this.listFields.FormattingEnabled = true;
+            this.listFields.HorizontalScrollbar = true;
+            this.listFields.ItemHeight = 12;
+            this.listFields.Location = new System.Drawing.Point(843, 125);
+            this.listFields.Name = "listFields";
+            this.listFields.Size = new System.Drawing.Size(127, 148);
+            this.listFields.TabIndex = 13;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 494);
+            this.Controls.Add(this.listFields);
+            this.Controls.Add(this.txtFieldName);
+            this.Controls.Add(this.btnAttributeQuery);
+            this.Controls.Add(this.btnSpatialQuery);
+            this.Controls.Add(this.btnTextElement);
+            this.Controls.Add(this.btnRender);
+            this.Controls.Add(this.btnAddData);
+            this.Controls.Add(this.btnOpenMap);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.axToolbarControl1);
+            this.Controls.Add(this.axMapControl1);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Controls.Add(this.axTOCControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GMap";
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tool_zoomin;
+        private System.Windows.Forms.ToolStripButton tool_zoomout;
+        private System.Windows.Forms.ToolStripButton tool_pan;
+        private System.Windows.Forms.Button btnOpenMap;
+        private System.Windows.Forms.Button btnAddData;
+        private System.Windows.Forms.Button btnRender;
+        private System.Windows.Forms.Button btnTextElement;
+        private System.Windows.Forms.Button btnSpatialQuery;
+        private System.Windows.Forms.Button btnAttributeQuery;
+        private System.Windows.Forms.TextBox txtFieldName;
+        private System.Windows.Forms.ListBox listFields;
+    }
+}
+
---
+++ GMap/MainForm.cs
@@ -0,0 +1,345 @@
+﻿//****************
+//****************
+//作者：黄铜
+//日期:2017.06.02
+//注意：代码中的字段名称需根据使用的具体数据做出相应更改
+//****************
+//*****************
+
+using System;
+using System.Windows.Forms;
+
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.DataSourcesFile;
+
+
+namespace GMap
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //鼠标操作
+        string pMouseOperate = null;
+
+        //放大
+        private void tool_zoomin_Click(object sender, EventArgs e)
+        {
+            axMapControl1.CurrentTool = null;
+            pMouseOperate = "ZoomIn";
+            axMapControl1.MousePointer = esriControlsMousePointer.esriPointerZoomIn;
+        }
+
+        //缩小
+        private void tool_zoomout_Click(object sender, EventArgs e)
+        {
+            axMapControl1.CurrentTool = null;
+            pMouseOperate = "ZoomOut";
+            axMapControl1.MousePointer = esriControlsMousePointer.esriPointerZoomOut;
+        }
+
+        //漫游
+        private void tool_pan_Click(object sender, EventArgs e)
+        {
+            axMapControl1.CurrentTool = null;
+            pMouseOperate = "Pan";
+            axMapControl1.MousePointer = esriControlsMousePointer.esriPointerPan;
+        }
+
+        //鼠标点击事件
+        private void axMapControl1_OnMouseDown(object sender, IMapControlEvents2_OnMouseDownEvent e)
+        {
+            if (pMouseOperate == "ZoomIn")
+            {
+                axMapControl1.Extent = axMapControl1.TrackRectangle();
+            }
+            if (pMouseOperate == "ZoomOut")
+            {
+                IEnvelope penv = axMapControl1.Extent;
+                penv.Expand(2, 2, true);
+                axMapControl1.Extent = penv;
+            }
+            if (pMouseOperate == "Pan")
+            {
+                axMapControl1.Pan();
+            }
+            if (pMouseOperate == "Identify")
+            {
+                IPoint point = new ESRI.ArcGIS.Geometry.Point();
+                point.PutCoords(e.mapX, e.mapY);
+                IFeatureLayer pFeatureLayer = axMapControl1.get_Layer(0) as IFeatureLayer;
+                IFeatureClass pFeatureClass = pFeatureLayer.FeatureClass;
+                string shapefieldname = pFeatureClass.ShapeFieldName;
+                ESRI.ArcGIS.Geodatabase.ISpatialFilter pSpatialFilter = new ESRI.ArcGIS.Geodatabase.SpatialFilter();
+                pSpatialFilter.Geometry = point;
+                pSpatialFilter.SpatialRel = ESRI.ArcGIS.Geodatabase.esriSpatialRelEnum.esriSpatialRelIntersects;
+                pSpatialFilter.set_OutputSpatialReference(shapefieldname, axMapControl1.SpatialReference);
+                pSpatialFilter.GeometryField = shapefieldname;
+                ESRI.ArcGIS.Geodatabase.IFeatureCursor pFeatureCursor = pFeatureClass.Search(pSpatialFilter, false);
+                ESRI.ArcGIS.Geodatabase.IFeature pFeature = pFeatureCursor.NextFeature();
+                if (pFeature != null)
+                {
+                    axMapControl1.FlashShape(pFeature.Shape);
+                }
+                IFields pField = pFeature.Fields;
+                listFields.Items.Clear();
+                for (int i = 0; i <= pField.FieldCount - 1; i++)
+                {
+                    listFields.Items.Add(pField.get_Field(i).Name + "=" + pFeature.get_Value(i));
+                }
+            }
+
+        }
+
+        //打开地图文档
+        private void btnOpenMap_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+                pOpenFileDialog.CheckFileExists = true;
+                pOpenFileDialog.Title = "打开地图文档";
+                pOpenFileDialog.Filter = "ArcMap文档(*.mxd)|*.mxd;|ArcMap模板(*.mxt)|*.mxt|发布地图文件(*.pmf)|*.pmf|所有地图格式(*.mxd;*.mxt;*.pmf)|*.mxd;*.mxt;*.pmf";
+                pOpenFileDialog.Multiselect = false;
+                pOpenFileDialog.RestoreDirectory = true;
+                if (pOpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string pFileName = pOpenFileDialog.FileName;
+                    if (pFileName == "")
+                    {
+                        return;
+                    }
+
+                    //检查地图文档有效性
+                    if (axMapControl1.CheckMxFile(pFileName))
+                    {
+                        //将数据载入pMapDocument并与Map控件关联
+                        IMapDocument pMapDocument = new MapDocument();
+                        pMapDocument.Open(pFileName, "");
+
+                        //获取Map中激活的地图文档
+                        axMapControl1.Map = pMapDocument.ActiveView.FocusMap;
+                        axMapControl1.ActiveView.Refresh();
+                    }
+                    else
+                    {
+                        MessageBox.Show(pFileName + "是无效的地图文档!", "信息提示");
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("打开地图文档失败" + ex.Message);
+            }
+        }
+
+        //添加数据
+        private void btnAddData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+                pOpenFileDialog.CheckFileExists = true;
+                pOpenFileDialog.Title = "打开Shape文件";
+                pOpenFileDialog.Filter = "Shape文件（*.shp）|*.shp";
+                pOpenFileDialog.ShowDialog();
+
+                IWorkspaceFactory pWorkspaceFactory;
+                IFeatureWorkspace pFeatureWorkspace;
+                IFeatureLayer pFeatureLayer;
+
+                string pFullPath = pOpenFileDialog.FileName;
+                if (pFullPath == "") return;
+
+                int pIndex = pFullPath.LastIndexOf("\\");
+
+                //文件路径
+                string pFilePath = pFullPath.Substring(0, pIndex);
+
+                //文件名
+                string pFileName = pFullPath.Substring(pIndex + 1);
+
+                //实例化ShapefileWorkspaceFactory工作空间，打开Shape文件
+                pWorkspaceFactory = new ShapefileWorkspaceFactory();
+                pFeatureWorkspace = (IFeatureWorkspace)pWorkspaceFactory.OpenFromFile(pFilePath, 0);
+
+                //创建并实例化要素集
+                IFeatureClass pFeatureClass = pFeatureWorkspace.OpenFeatureClass(pFileName);
+                pFeatureLayer = new FeatureLayer();
+                pFeatureLayer.FeatureClass = pFeatureClass;
+                pFeatureLayer.Name = pFeatureLayer.FeatureClass.AliasName;
+
+                axMapControl1.Map.AddLayer(pFeatureLayer);
+                axMapControl1.ActiveView.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("图层加载失败！" + ex.Message);
+            }
+        }
+
+        //地图渲染
+        private void btnRender_Click(object sender, EventArgs e)
+        {
+            IGeoFeatureLayer PGeoFeatureLayer = (IGeoFeatureLayer)axMapControl1.get_Layer(0);
+            IUniqueValueRenderer pUniqueValueRenderer = CreateTrackUniqueValueRenderer(PGeoFeatureLayer.FeatureClass, "TYPE");
+
+            PGeoFeatureLayer.Renderer = (IFeatureRenderer)pUniqueValueRenderer;
+
+            axMapControl1.ActiveView.Refresh();
+            axTOCControl1.SetActiveView(axTOCControl1.ActiveView);
+        }
+
+        //唯一值渲染
+        private IUniqueValueRenderer CreateTrackUniqueValueRenderer(IFeatureClass featureClass, string fieldName)
+        {
+            IRgbColor color = new RgbColor();
+            color.Red = 0;
+            color.Blue = 0;
+            color.Green = 255;
+
+            ISimpleLineSymbol simpleLineSymbol = new SimpleLineSymbol();
+            simpleLineSymbol.Color = (IColor)color;
+            simpleLineSymbol.Style = esriSimpleLineStyle.esriSLSSolid;
+            simpleLineSymbol.Width = 1.0;
+
+            IUniqueValueRenderer uniqueRenderer = new UniqueValueRenderer();
+            uniqueRenderer.FieldCount = 1;
+            uniqueRenderer.set_Field(0, fieldName);
+            uniqueRenderer.DefaultSymbol = (ISymbol)simpleLineSymbol;
+            uniqueRenderer.UseDefaultSymbol = true;
+
+            Random rand = new Random();
+            bool bValFound = false;
+            IFeatureCursor featureCursor = featureClass.Search(null, true);
+            IFeature feature = null;
+            string val = string.Empty;
+            int fieldID = featureClass.FindField(fieldName);
+            if (-1 == fieldID)
+                return uniqueRenderer;
+
+            while ((feature = featureCursor.NextFeature()) != null)
+            {
+                bValFound = false;
+                val = Convert.ToString(feature.get_Value(fieldID));
+                for (int i = 0; i < uniqueRenderer.ValueCount - 1; i++)
+                {
+                    if (uniqueRenderer.get_Value(i) == val)
+                        bValFound = true;
+                }
+
+                if (!bValFound)//need to add the value to the renderer
+                {
+                    color.Red = rand.Next(255);
+                    color.Blue = rand.Next(255);
+                    color.Green = rand.Next(255);
+
+                    simpleLineSymbol = new SimpleLineSymbol();
+                    simpleLineSymbol.Color = (IColor)color;
+                    simpleLineSymbol.Style = esriSimpleLineStyle.esriSLSSolid;
+                    simpleLineSymbol.Width = 1.0;
+
+                    uniqueRenderer.AddValue(val, "name", (ISymbol)simpleLineSymbol);
+                }
+            }
+
+            ESRI.ArcGIS.ADF.ComReleaser.ReleaseCOMObject(featureCursor);
+
+            return uniqueRenderer;
+        }
+      
+        //文本标注
+        private void btnTextElement_Click(object sender, EventArgs e)
+        {
+            if (axMapControl1.LayerCount > 0)
+            {
+                IGeoFeatureLayer pGeoFeatureLayer;
+                ILineLabelPosition pLineLabelPosition;
+                ILabelEngineLayerProperties pLabelEngineLayerProperties;
+                IAnnotateLayerProperties pAnnotateLayerProperties;
+
+                pGeoFeatureLayer = (IGeoFeatureLayer)axMapControl1.get_Layer(0);
+                pGeoFeatureLayer.AnnotationProperties.Clear();
+
+                pLineLabelPosition = new LineLabelPosition();
+                pLineLabelPosition.Above = false;
+                pLineLabelPosition.AtEnd = false;
+                pLineLabelPosition.Below = false;
+                pLineLabelPosition.Horizontal = false;
+                pLineLabelPosition.InLine = true;
+                pLineLabelPosition.OnTop = true;
+                pLineLabelPosition.Parallel = true;
+                pLineLabelPosition.ProduceCurvedLabels = true;
+
+                ITextSymbol pTextSymbol = new TextSymbol();
+                IColor pColor = new RgbColor();
+                pColor.RGB = 100;
+                pTextSymbol.Size = 12;
+                pTextSymbol.Font.Name = "宋体";
+                pTextSymbol.Color = pColor;
+
+                pLabelEngineLayerProperties = new LabelEngineLayerProperties() as ILabelEngineLayerProperties;
+                pLabelEngineLayerProperties.Symbol = pTextSymbol;
+                pLabelEngineLayerProperties.IsExpressionSimple = true;
+                pLabelEngineLayerProperties.Expression = "[DESCRIP]";//需要中括号,这里显示NAME字段属性
+                pLabelEngineLayerProperties.BasicOverposterLayerProperties.LineLabelPosition = pLineLabelPosition;
+
+                pAnnotateLayerProperties = (IAnnotateLayerProperties)pLabelEngineLayerProperties;
+                pAnnotateLayerProperties.DisplayAnnotation = true;
+                pAnnotateLayerProperties.FeatureLayer = pGeoFeatureLayer;
+
+                pAnnotateLayerProperties.LabelWhichFeatures = esriLabelWhichFeatures.esriAllFeatures;
+                pAnnotateLayerProperties.WhereClause = "";
+
+                pGeoFeatureLayer.AnnotationProperties.Add(pAnnotateLayerProperties);
+                pGeoFeatureLayer.DisplayAnnotation = true;
+                axMapControl1.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGraphics, null, null);
+                axMapControl1.ActiveView.Refresh();
+            }
+        }
+
+        //空间查询
+        private void btnSpatialQuery_Click(object sender, EventArgs e)
+        {
+            axMapControl1.CurrentTool = null;
+            pMouseOperate = "Identify";
+            axMapControl1.MousePointer = esriControlsMousePointer.esriPointerIdentify;
+        }
+
+        //属性查询
+        private void btnAttributeQuery_Click(object sender, EventArgs e)
+        {
+
+            IFeatureLayer pFeatureLayer = axMapControl1.get_Layer(0) as IFeatureLayer;
+            IFeatureClass pFeatureClass = pFeatureLayer.FeatureClass;
+            IQueryFilter pQueryFilter = new QueryFilter();
+            pQueryFilter.WhereClause = "DESCRIP='" + txtFieldName.Text.ToString() + "'";
+            IFeatureCursor pFeatureCursor = pFeatureClass.Search(pQueryFilter, false);
+            IFeature pFeature = pFeatureCursor.NextFeature();
+            axMapControl1.Map.ClearSelection();
+
+            if (pFeature != null)
+            {
+                axMapControl1.FlashShape(pFeature.Shape);
+            }
+            axMapControl1.Map.SelectFeature(pFeatureLayer, pFeature);
+            axMapControl1.ActiveView.Refresh();
+            IFields pField = pFeature.Fields;
+            listFields.Items.Clear();
+            for (int i = 0; i <= pField.FieldCount - 1; i++)
+            {
+                listFields.Items.Add(pField.get_Field(i).Name + "=" + pFeature.get_Value(i));
+            }
+        }
+
+
+    }
+}
